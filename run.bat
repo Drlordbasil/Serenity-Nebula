e190fd2df09b61347dc916fbbeef2d84b1ad8a07
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-08-19 21:59:45 -0400

    Update run.bat
    need to fix this in god fathers program.

--- run.bat
+++ run.bat
@@ -1,6 +1,4 @@
-Sure! Here's an example of what the run.bat file could look like:
 
-```
 @echo off
 
 echo Installing required packages...
@@ -10,6 +8,3 @@ echo Running program...
 python main.py
 
 echo Program finished running.
-```
-
-Make sure that both the main.py file and the requirements.txt file are in the same directory as the run.bat file. When the user double-clicks on the run.bat file, it will install the required packages and then run the main.py program.
