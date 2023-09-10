dbeddb760f68d5534ad345ceef319c5bd36e1bf7
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-09 23:31:58 -0400

    Fix syntax error

--- AI_CoPilot.py
+++ AI_CoPilot.py
@@ -1,21 +1,23 @@
 
 import subprocess
 
+
 def run_program():
     try:
         subprocess.run(["python", "main.py"], check=True)
         print("Program executed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error executing program: {e}")
-        
+
+
 def main():
     print("Welcome to the AI chatbot!")
     print("This chatbot will help you run the program locally on your PC.")
     print("Are you ready to run the program? (yes/no)")
-    
+
     while True:
         choice = input("> ")
-        
+
         if choice.lower() == "yes":
             run_program()
             break
@@ -25,6 +27,7 @@ def main():
         else:
             print("Invalid choice, please try again.")
             continue
+
 
 if __name__ == "__main__":
     main()
